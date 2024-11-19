4487895b66713c837d9f78467b9142b92b3de416
umerfayyaz2~ <ufayyaz19@gmail.com>
2024-11-19 15:12:24 +0500

    Updated main and pro file to integrate it with styles.qss file

--- The_Word_Whisperer_App/main.cpp
+++ The_Word_Whisperer_App/main.cpp
@@ -1,8 +1,18 @@
 #include <QApplication>
 #include "TheWordWhispererUI.h"
+#include <QFile>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    // Load the stylesheet
+    QFile file(":/styles.qss");
+    if (file.open(QFile::ReadOnly)) {
+        QString stylesheet = QLatin1String(file.readAll());
+        app.setStyleSheet(stylesheet);
+    } else {
+        qWarning("Could not open styles.qss file!");
+    }
 
     TheWordWhispererUI window;
     window.show();
